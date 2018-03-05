a7b0c9dd7d858a5afd421d35da712920389037d4
Piotr Zielinski <204037@edu.p.lodz.pl>
2018-03-05 22:13:55 +0100

    SyganlCiagly generuje poprawne sygnaly; Testy generuja poprawne wykresy

--- Generator/SygnalCiagly.py
+++ Generator/SygnalCiagly.py
@@ -13,26 +13,34 @@ class SygnalCiagly(object):
         self.d = czas_trwania
         self.f_p = f_probkowania
         self._czas_trwania_czystego_sygnalu = self.d - self.t1
-        self._ilosc_probek_wlasciwych = int(self._czas_trwania_czystego_sygnalu / self.f_p)
+        self._ilosc_probek_wlasciwych = int(self._czas_trwania_czystego_sygnalu / self.f_p) + 1
         self.T = okres
+        self.y = None  # inicjalizowane w klasach potomnych
+        self.x = np.arange(0, czas_trwania + f_probkowania, f_probkowania)
 
-    def generuj(self):
+    def _generuj_probki(self):
         """
-        :return: ndarray of samples
+        Inicjalizuje self.x ndarray próbek
         """
         raise NotImplementedError
 
+    def generuj_uklad_xy(self):
+        """
+        :return: tuple(x(t), t)
+        """
+        return self.x, self.y
+
     def srednia(self):
-        return np.sum(self.x*self.f_p)/len(self.x)
+        return np.sum(self.x/self.f_p)/len(self.x)
 
     def srednia_bezwzgledna(self):
-        return np.sum(np.abs(self.x*self.f_p)) / len(self.x)
+        return np.sum(np.abs(self.x/self.f_p)) / len(self.x)
 
     def moc_srednia(self):
-        return np.sum((self.x**2)*self.f_p) / len(self.x)
+        return np.sum((self.x**2)/self.f_p) / len(self.x)
 
     def wariancja_wokol_sredniej(self):
-        return np.sum(((self.x-self.srednia)**2)*self.f_p) / len(self.x)
+        return np.sum(((self.x-self.srednia)**2)/self.f_p) / len(self.x)
 
     def wartosc_skuteczna(self):
         return np.sqrt(self.moc_srednia())
@@ -44,9 +52,11 @@ class SzumORozkladzieJednostajnym(SygnalCiagly):
     def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
         super(SzumORozkladzieJednostajnym, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         return np.append(poczatkowe_zera, 2 * self.A * np.random.ranf(self._ilosc_probek_wlasciwych) - self.A)
 
 
@@ -54,9 +64,11 @@ class SzumGaussowski(SygnalCiagly):
     def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
         super(SzumGaussowski, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         return np.append(poczatkowe_zera, np.random.randn(self._ilosc_probek_wlasciwych))
 
 
@@ -64,9 +76,11 @@ class Sinusoida(SygnalCiagly):
     def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
         super(Sinusoida, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         t = np.arange(0, self._czas_trwania_czystego_sygnalu + self.f_p, self.f_p)
         return np.append(poczatkowe_zera, self.A * np.sin(t))
 
@@ -75,9 +89,11 @@ class SinusoidaWyprostowanaJednopolowkowo(SygnalCiagly):
     def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
         super(SinusoidaWyprostowanaJednopolowkowo, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         t = np.arange(0, self._czas_trwania_czystego_sygnalu + self.f_p, self.f_p)
         x_sin = np.sin(t)
         return np.append(poczatkowe_zera, 0.5 * self.A * (x_sin + np.abs(x_sin)))
@@ -87,11 +103,13 @@ class SinusoidaWyprostowanaDwupolowkowo(SygnalCiagly):
     def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
         super(SinusoidaWyprostowanaDwupolowkowo, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         t = np.arange(0, self._czas_trwania_czystego_sygnalu + self.f_p, self.f_p)
-        return np.append(poczatkowe_zera, np.abs(np.sin(t)))
+        return np.append(poczatkowe_zera, np.abs(self.A * np.sin(t)))
 
 
 class Prostokat(SygnalCiagly):
@@ -99,40 +117,69 @@ class Prostokat(SygnalCiagly):
         super(Prostokat, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
         self.k_w = wspolczynnik_wypelnienia
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
         x = np.empty(self._ilosc_probek_wlasciwych)
         for i in range(self._ilosc_probek_wlasciwych):
-            t = self.f_p * i
+            t = self.t1 + self.f_p * i
             k = int((i * self.f_p)/self.T)
-            if (k*self.T + self.t1) <= t < (self.k_w*self.T + k*self.T + self.t1):
-                x[i] = 1
+            arg0 = (k*self.T + self.t1)
+            arg1 = (self.k_w*self.T + k*self.T + self.t1)
+            if arg0 <= t < arg1:
+                x[i] = self.A
             else:
                 x[i] = 0
         return np.append(poczatkowe_zera, x)
 
 
 class ProstokatSymetryczny(SygnalCiagly):
-    def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
+    def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania, wspolczynnik_wypelnienia):
         super(ProstokatSymetryczny, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.k_w = wspolczynnik_wypelnienia
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
-        return np.append(poczatkowe_zera)
-        raise NotImplementedError()
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
+        x = np.empty(self._ilosc_probek_wlasciwych)
+        for i in range(self._ilosc_probek_wlasciwych):
+            t = self.t1 + self.f_p * i
+            k = int((i * self.f_p)/self.T)
+            arg0 = (k*self.T + self.t1)
+            arg1 = (self.k_w*self.T + k*self.T + self.t1)
+            if arg0 <= t < arg1:
+                x[i] = self.A
+            else:
+                x[i] = -self.A
+        return np.append(poczatkowe_zera, x)
 
 
 class Trojkat(SygnalCiagly):
-    def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania):
+    def __init__(self, amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania, wspolczynnik_wypelnienia):
         super(Trojkat, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
+        self.k_w = wspolczynnik_wypelnienia
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)).fill(0)
-        return np.append(poczatkowe_zera)
-        raise NotImplementedError()
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p))
+        poczatkowe_zera.fill(0)
+        x = np.empty(self._ilosc_probek_wlasciwych)
+        for i in range(self._ilosc_probek_wlasciwych):
+            t = self.t1 + self.f_p * i
+            k = int((i * self.f_p)/self.T)
+            arg0 = (k*self.T + self.t1)
+            arg1 = (self.k_w*self.T + k*self.T + self.t1)
+            wspolczynnik_do_wyliczania_x = (t - k * self.T - self.t1)
+            if arg0 <= t < arg1:
+                x[i] = self.A / (self.k_w * self.T) * wspolczynnik_do_wyliczania_x
+            else:
+                x[i] = -self.A / (self.T * (1 - self.k_w)) * wspolczynnik_do_wyliczania_x + self.A / (1 - self.k_w)
+        return np.append(poczatkowe_zera, x)
 
 
 class SkokJednostkowy(SygnalCiagly):
@@ -140,9 +187,12 @@ class SkokJednostkowy(SygnalCiagly):
         super(SkokJednostkowy, self).\
             __init__(amplituda, czas_poczatkowy, czas_trwania, okres, f_probkowania)
         self.t_s = t_skoku
+        self.y = self._generuj_probki()
 
-    def generuj(self):
-        poczatkowe_zera = np.empty(int(self.t1/self.f_p)+int((self.t_s-self.t1)/self.f_p)).fill(0)
+    def _generuj_probki(self):
+        poczatkowe_zera = np.empty(int(self.t1/self.f_p)+int((self.t_s-self.t1)/self.f_p))
+        poczatkowe_zera.fill(0)
         poczatkowe_zera[-1] = self.A/2
-        koncowe_zera = np.empty(int(self.d-self.t_s)).fill(self.A)
+        koncowe_zera = np.empty(int((self.d-self.t_s+self.f_p)/self.f_p))
+        koncowe_zera.fill(self.A)
         return np.append(poczatkowe_zera, koncowe_zera)
--- Wykresy/GeneratorWykresow.py
+++ Wykresy/GeneratorWykresow.py
@@ -1,6 +1,28 @@
 from Generator import SygnalDyskretny, SygnalCiagly
 import matplotlib.pyplot as plt
 
-sig = SygnalCiagly.Prostokat(10, 0.4, 10, 0.5, 0.01, 0.75)  # nie dziala argument z czasem poczatkowym
-plt.plot(sig.generuj())
-plt.show()
+
+def test():
+    config = (10, 2, 10, 0.5, 0.01)
+    plt.plot(*SygnalCiagly.Prostokat(*config, 0.75).generuj_uklad_xy())
+    plt.plot(*SygnalCiagly.Sinusoida(*config).generuj_uklad_xy())
+    plt.plot(*SygnalCiagly.ProstokatSymetryczny(*config, 0.25).generuj_uklad_xy())
+    plt.plot(*SygnalCiagly.SinusoidaWyprostowanaDwupolowkowo(*config).generuj_uklad_xy())
+    plt.plot(*SygnalCiagly.SinusoidaWyprostowanaJednopolowkowo(*config).generuj_uklad_xy())
+    plt.plot(*SygnalCiagly.Trojkat(*config, 0.5).generuj_uklad_xy())
+    plt.show()
+
+    plt.plot(*SygnalCiagly.SkokJednostkowy(*config, 1).generuj_uklad_xy())
+    plt.show()
+
+    plt.plot(*SygnalCiagly.SzumORozkladzieJednostajnym(*config).generuj_uklad_xy())
+    plt.show()
+
+    plt.plot(*SygnalCiagly.SzumGaussowski(*config).generuj_uklad_xy())
+    plt.show()
+    pass
+
+
+if __name__ == "__main__":
+    print("Tego sie tak nie uruchamia")
+    test()
